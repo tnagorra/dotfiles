6fda82c419cdd2935d4edb916f549bcb0d6af2e6
tnagorra <weathermist@gmail.com>
2022-07-12 10:39:03 +0545

    Update live_grep_raw to live_grep_args

--- nvim/.config/nvim/lua/_telescope.lua
+++ nvim/.config/nvim/lua/_telescope.lua
@@ -1,5 +1,5 @@
 local map = vim.keymap.set
 
-require('telescope').load_extension('live_grep_raw')
+require('telescope').load_extension('live_grep_args')
 map('n', '<leader>f', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', { silent = true })
-map('n', '<leader>F', ':lua require("telescope").extensions.live_grep_raw.live_grep_raw()<CR>', { silent = true })
+map('n', '<leader>F', ':lua require("telescope").extensions.live_grep_args.live_grep_args()<CR>', { silent = true })
